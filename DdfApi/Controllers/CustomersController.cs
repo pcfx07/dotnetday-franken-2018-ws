583b985222b42de8be29973a8a1449d6d65e4f92
Pawel Gerr <pawel.gerr@thinktecture.com>
2018-04-27 13:01:44 +0200

    Customers controller

---
+++ DdfApi/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using DdfApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DdfApi.Controllers
+{
+   [Route("api/[controller]")]
+   public class CustomersController : Controller
+   {
+      private static readonly List<Customer> _customers = new List<Customer>()
+                                                 {
+                                                    new Customer()
+                                                    {
+                                                       Id = Guid.NewGuid(),
+                                                       Age = 25,
+                                                       FirstName = "John",
+                                                       LastName = "Smith"
+                                                    },
+                                                    new Customer()
+                                                    {
+                                                       Id = Guid.NewGuid(),
+                                                       Age = 23,
+                                                       FirstName = "Jane",
+                                                       LastName = "Doe"
+                                                    },
+                                                 };
+
+      /// <summary>
+      /// Fetches all customers.
+      /// </summary>
+      /// <returns>A collection of customers.</returns>
+      /// <response code="200">Fetched customers successfully.</response>
+      [HttpGet]
+      public IEnumerable<Customer> GetAll()
+      {
+         return _customers;
+      }
+   }
+}
---
+++ DdfApi/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// ReSharper disable UnusedMember.Global
+
+namespace DdfApi.Models
+{
+   /// <summary>
+   /// A customer.
+   /// </summary>
+   public class Customer
+   {
+      /// <summary>
+      /// Id of the customer.
+      /// </summary>
+      public Guid Id { get; set; }
+
+      /// <summary>
+      /// The first name of the customer.
+      /// </summary>
+      public string FirstName { get; set; }
+
+      /// <summary>
+      /// The last name of the customer.
+      /// </summary>
+      public string LastName { get; set; }
+
+      /// <summary>
+      /// The age of the customer.
+      /// </summary>
+      public int Age { get; set; }
+   }
+}
